23884d69f3342408b510e1e704a2262148160e05
CarolinaDougnac <carolinadougnacv@gmail.com>
2020-07-26 23:49:42 -0400

    Primera version Proyecto2020

---
+++ Proyecto2020/src/InterfazGrafica.java
@@ -0,0 +1,54 @@
+import java.awt.EventQueue;
+
+import javax.swing.JOptionPane;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class InterfazGrafica {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InterfazGrafica window = new InterfazGrafica();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public InterfazGrafica() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JButton btnNewButton = new JButton("Mensaje");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Este es un mensaje del proyecto 2020 antes de GIT");
+			}
+		});
+		frame.getContentPane().add(btnNewButton, BorderLayout.SOUTH);
+	}
+
+}
